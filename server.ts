82fc513de0408f68b6df790e3056e205e03d8e2d
GilAvraham2909 <gilavraham2909@gmail.com>
2023-02-08 16:02:10 +0200

    add error handeling

--- server.ts
+++ server.ts
@@ -15,27 +15,59 @@ function isValidHttpUrl(string) {
   return url.protocol === "http:" || url.protocol === "https:";
 }
 
-app.get('/encode', (req, res) => {
-    var long_url = req.query.url;
-    // see if its a valid URL
-    if (!isValidHttpUrl(long_url)){
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify({ "error:": "not a valid URL" }));
-    }
-    // generate a short url and sore it im memory.
-    var shorted_url = generator.encode(long_url);
+app.get('/', (req, res) => {
+  try{
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ "shortURL:": shorted_url }));
-    
+    res.end(JSON.stringify({ "URL shortening service:": [
+      "To get short url for your url do: /encode?url=[url_to_be_shorten]",
+      "To get the long URL you have shorten do: /encode?url=[given_shorten_url]"
+    ] }));
+  } catch (error){
+    console.log(error);
+  } 
+})
+
+app.get('/encode', (req, res) => {
+  try{
+      var long_url = req.query.url;
+      // if we could not get url parameter.
+      if (!long_url){
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ "error:": "please add URL parameters name url. example: /encode?url=[url_to_be_shorten]" }));
+        return;
+      }
+      // see if its a valid URL
+      if (!isValidHttpUrl(long_url)){
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ "error:": "not a valid URL" }));
+        return;
+      }
+      // generate a short url and sore it im memory.
+      var shorted_url = generator.encode(long_url);
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ "shortURL:": shorted_url }));
+  } catch (error){
+    console.log(error);
+  }    
 })
 
 app.get('/decode', (req, res) => {
-    var shorted_url = req.query.url;
-    // get the long url that we stored.
-    var long_url = generator.decode(shorted_url);
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ "longURL": long_url }));
-  })
+  try{
+      var shorted_url = req.query.url;
+      // if we could not get url parameter.
+      if (!long_url){
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ "error:": "please add URL parameters name url. example: /encode?url=[given_shorten_url]" }));
+        return;
+      }
+      // get the long url that we stored.
+      var long_url = generator.decode(shorted_url);
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ "longURL": long_url }));
+  } catch (error){
+    console.log(error);
+  } 
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
